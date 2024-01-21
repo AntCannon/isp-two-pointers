a2788f36eaf8c78db46f397f13a6548c25bd5878
AntCannon <anthonycannonier@gmail.com>
2024-01-20 21:10:30 -0500

    add two pointer algo to nestVsTwoP.js

--- nestVsTwoP.js
+++ nestVsTwoP.js
@@ -10,11 +10,42 @@ function twoSumNL(sArr, target) {
       if (sum === target) return [i, j];
     }
   }
-  return 'No possible pairs';
+  return 'NL NO SATISFACTORY PAIRS';
 }
 
-console.log(`arr arr1K min and max:`, gen.arr1K[0], gen.arr1K[Math.floor((gen.arr1K.length-1)/2)], gen.arr1K[gen.arr1K.length-1]);
+function twoSumTP(sArr, target) {
+  let leftPointer = 0;
+  let rightPointer = sArr.length-1;
+  
+  while (leftPointer < rightPointer) {
+    const sum = sArr[leftPointer] + sArr[rightPointer];
+  
+    if (sum === target) {
+    return [leftPointer, rightPointer];
+    } else if (sum < target) {
+    leftPointer++;
+    } else {
+    rightPointer--;
+    }
+  }
+  
+  return 'TP NO SATISFACTORY PAIRS'
+  }
+
+console.log(`arr arr1K min, mid & max:`,
+gen.arr1K[0],
+gen.arr1K[Math.floor((gen.arr1K.length-1)/2)],
+gen.arr1K[gen.arr1K.length-1], '\n');
+
+// nested loop VS. two pointer test case: 1K
+const target1K = 8753;
 console.log(`NL start:`, start = Date.now());
-console.log(twoSumNL(gen.arr1K, 9980));
+console.log(twoSumNL(gen.arr1K, target1K));
 console.log(`NL finish:`, finish = Date.now());
-console.log(`NL arr runtime:`, finish - start);
+console.log(`NL arr runtime:`, finish - start, '\n');
+
+console.log(`TP start:`, start = Date.now());
+console.log(twoSumTP(gen.arr1K, target1K));
+console.log(`TP finish:`, finish = Date.now());
+console.log(`TP arr runtime:`, finish - start, '\n');
+
