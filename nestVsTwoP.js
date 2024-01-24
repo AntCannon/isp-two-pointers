67ceb25359c87afbe40bdbb158cac085fd1a3fe8
AntCannon <anthonycannonier@gmail.com>
2024-01-24 12:31:57 -0500

    delete static runtime comparisons nestedVsTwoP.js

--- nestVsTwoP.js
+++ nestVsTwoP.js
@@ -1,6 +1,5 @@
 // use this to compare the run time of the nested loop solution and the two pointer algo solution for two sum problems of arrays of different  exponent form lengths.
 
-
 const gen = require('./genArr.js');
 
 // twoSum problem with nested loop return the indices.
@@ -34,90 +33,6 @@ function twoSumTP(sArr, target) {
   
   return 'TP NO SATISFACTORY PAIRS'
   }
-
-
-// ------------------- 1K
-
-
-// // nested loop VS. two pointer test case: 1K
-// // arr data
-// console.log(`arr1K min, mid & max:`,
-// gen.arr1K[0],
-// gen.arr1K[Math.floor((gen.arr1K.length-1)/2)],
-// gen.arr1K[gen.arr1K.length-1], '\n');
-
-// // Nested loop run time
-// const target1K = 50;
-// console.log(`NL start:`, start = Date.now());
-// console.log(twoSumNL(gen.arr1K, target1K));
-// console.log(`NL finish:`, finish = Date.now());
-// console.log(`NL arr runtime:`, finish - start, '\n');
-
-// // Two pointer run time
-// console.log(`TP start:`, start = Date.now());
-// console.log(twoSumTP(gen.arr1K, target1K));
-// console.log(`TP finish:`, finish = Date.now());
-// console.log(`TP arr runtime:`, finish - start, '\n');
-
-
-// // ------------------- 10K
-
-
-// // nested loop VS. two pointer test case: 10K
-// // arr data
-// console.log(`arr arr10K length, min, mid & max:`,
-// gen.arr10K.length,
-// gen.arr10K[0],
-// gen.arr10K[Math.floor((gen.arr10K.length-1)/2)],
-// gen.arr10K[gen.arr10K.length-1], '\n');
-// // console.log(gen.arr10K);
-
-// // Nested loop run time
-// const target10K = 90000;
-// console.log(`NL start:`, start = Date.now());
-// console.log(twoSumNL(gen.arr10K, target10K));
-// console.log(`NL finish:`, finish = Date.now());
-// console.log(`NL arr runtime:`, finish - start, '\n');
-
-// // Two pointer run time
-// console.log(`TP start:`, start = Date.now());
-// console.log(twoSumTP(gen.arr10K, target10K));
-// console.log(`TP finish:`, finish = Date.now());
-// console.log(`TP arr runtime:`, finish - start, '\n');
-
-
-// ------------------- 100K
-
-
-// // nested loop VS. two pointer test case: 100K
-// // arr data
-// console.log(`\narr100K data`,
-// `\nlength:`, gen.arr100K.length,
-// `\nmin:`, gen.arr100K[0],
-// `\nmid`, gen.arr100K[Math.floor((gen.arr100K.length-1)/2)],
-// `\nmax`, gen.arr100K[gen.arr100K.length-1],
-// `\nmin sum:`, gen.arr100K[0] + gen.arr100K[1],
-// `\nmax sum:`, gen.arr100K[gen.arr100K.length-2]+ gen.arr100K[gen.arr100K.length-1],'\n');
-// // console.log(gen.arr10K);
-
-// // Nested loop run time
-// const target100K = 999_922; //  lowest: ~30 mid: ~500_000 highest: ~999_980
-// console.log(`target100K`, target100K);
-// console.time('NL arr100K');
-// console.log(twoSumNL(gen.arr100K, target100K));
-// console.timeEnd('NL arr100K');
-
-// // Two pointer run time
-// console.time('TP arr100K');
-// console.log(twoSumTP(gen.arr100K, target100K));
-// console.timeEnd('TP arr100K');
-
-
-// ------------------- 1M
-
-
-// nested loop VS. two pointer test case: 100K
-// arr data
 
 const arrToTest = gen.arr10K;
 const arrLabel = 'arr10K';
